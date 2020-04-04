b60a1f8d31600942a6b1bc903f8729b19730db74
Jtrinity87 <mercado.jacqueline.87@gmail.com>
2020-04-04 00:19:58 -0400

    uploading incomplete assignment

---
+++ script.js
@@ -0,0 +1,5 @@
+//code that Brittany tried to help with
+// function addaCom() {
+//     let comLogo = prompt(`Copy and paste url of company logo:`);
+//     document.getElementById("demo").src = comLogo;
+//     }
